79045293438da12d79a5edde3a26929a0a13954c
gauthamksn <gsnair@adobe.com>
2025-05-02 17:51:42 +0530

    Adds Firebase and Netlify configuration files

---
+++ server/functions/app.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const serverless = require("serverless-http");
+const app = express();
+const router = express.Router();
+const admin = require('firebase-admin');
+
+const serviceAccount = require("../firebasekey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+function sendNotificationToDevice(token, message, res) {
+  const payload = {
+    data: {
+      title: 'Love you so much',
+      body: message,
+    },
+    notification: {
+      title: 'Love you so much',
+      body: message,
+    },
+
+    android: {
+      priority: "high",
+      notification: {
+        channelId: "high_priority_channel",
+      },
+    },
+    token: token,
+  };
+
+  admin.messaging().send(payload)
+    .then(response => {
+      console.log('Successfully sent message:', response);
+      res.send("Notification sent successfully!");
+    })
+    .catch(error => {
+      res.status(500).send("Error sending notification");
+      console.error('Error sending message:', error);
+    });
+}
+
+router.get("/", (req, res) => {
+  res.send("Hello from the serverless function!");
+});
+
+router.post("/", (req, res) => {
+  const { token, message } = JSON.parse(req.body);
+  if (!token || !message) {
+    return res.status(400).send("Token and message are required");
+  }
+  sendNotificationToDevice(token, message, res);
+});
+
+app.use("/.netlify/functions/app", router);
+module.exports.handler = serverless(app);
+
